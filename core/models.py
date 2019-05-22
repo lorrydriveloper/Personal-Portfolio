ddb9f6d998db5fe64138e53d53246c18488f4e88
reddevilcero <reddevil_cero@hotmail.com>
2019-05-22 11:57:20 +0100

    somo proof of back-end

--- core/models.py
+++ core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils.translation import gettext as _
 
 # Create your models here.
+
+
+class Learning(models.Model):
+
+    name = models.CharField(_("name"), max_length=50)
+    icon = models.ImageField(_("Icon"), upload_to="img/icons")
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = _("learning")
+        verbose_name_plural = _("learnings")
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("learning_detail", kwargs={"pk": self.pk})
--- core/views.py
+++ core/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
-
+from .models import Learning
 # Create your views here.
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
+    model = Learning
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["learning"] = Learning.objects.all()
+        context['hola'] = 'hola'
+        return context
+    
     
